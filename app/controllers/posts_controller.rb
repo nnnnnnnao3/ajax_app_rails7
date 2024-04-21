2e25b7014d68616e28d3405d77c8fd7d8a284935
nnnnnnnao3 <nnnnnnnao3@gmail.com>
2024-04-21 17:02:47 +0900

    復習のための再実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,8 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(content: params[:content])
+    # 新たに投稿されたメモの内容を変数postに格納
     render json:{ post: post }
+    #上記で定義した変数postの値を、postというキーとセットでJavaScriptに送信
   end
 end
--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,37 +1,39 @@
 const buildHTML = (XHR) => {
-  const item = XHR.response.post;
-  const html = `
-    <div class="post">
-      <div class="post-date">
-        投稿日時：${item.created_at}
-      </div>
-      <div class="post-content">
-        ${item.content}
-      </div>
-    </div>`;
+
+const item = XHR.response.post; //レスポンスの中から投稿されたメモの情報(post)を抽出し、変数itemに格納
+const html = `
+  <div class="post">
+    <div class="post-date">
+      投稿日時：${item.created_at}
+    </div>
+    <div class="post-content">
+      ${item.content}
+    </div>
+  </div>`;
   return html;
 };
 
-function post (){
-  const form = document.getElementById("form");
+
+function post (){  //ページが読み込まれたときイベント発火
+  const form = document.getElementById("form"); //id:formのHTML要素を変数formに格納
   form.addEventListener("submit", (e) => {
-    e.preventDefault();  
-    const formData = new FormData(form);
-    const XHR = new XMLHttpRequest();
-    XHR.open("POST", "/posts", true);
-    XHR.responseType = "json";
-    XHR.send(formData);
-    XHR.onload = () => {
+    e.preventDefault();//「投稿ボタンをクリックした」という動作を無効化
+    const formData = new FormData(form); //FormDataオブジェクトを使って取得したフォームの値を変数formDataに格納
+    const XHR = new XMLHttpRequest(); //新しくオブジェクトを生成
+    XHR.open("POST", "/posts", true); //リクエストの内容を指定
+    XHR.responseType = "json"; //データフォーマットを指定し"JSON"に設定
+    XHR.send(formData); //フォームに入力された内容をサーバー側に送信
+    XHR.onload = () => {//リクエストの送信が成功したら呼び出す
       if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         return null;
       };
-      const list = document.getElementById("list");
+      const list = document.getElementById("list");//id:listのHTML要素を取得
       const formText = document.getElementById("content");
-      list.insertAdjacentHTML("afterend", buildHTML(XHR));
-      formText.value = "";
-    };
-  }); 
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));  //htmlに格納したHTML要素をlistのHTML要素の直後に入れる
+         formText.value = ""; //フォームの中身をリセット
+      };
+  });
 };
 
- window.addEventListener('turbo:load', post);
+window.addEventListener('turbo:load', post);
